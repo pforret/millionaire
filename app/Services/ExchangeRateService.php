1bf414d20a505bd25f448b06c715f5aafd0eda95
Peter Forret <peter@forret.com>
2022-03-01 21:01:00 +0100

    ADD: Currency.php Rate.php ExchangeRateService.php 2022_03_01_194639_create_currencies_table.php 2022_03_01_194900_create_rates_table.php,

---
+++ app/Services/ExchangeRateService.php
@@ -0,0 +1,8 @@
+<?php
+
+class ExchangeRateService
+{
+    // via https://github.com/exchangeratesapi/exchangeratesapi
+    // URL: https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml
+
+}
