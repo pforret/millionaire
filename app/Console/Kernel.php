81785e1530b372c10c38d34eff2becdd6c075dac
Peter Forret <peter@forret.com>
2022-03-05 20:44:19 +0100

    MOD: Kernel.php VERSION.blade.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('million:rates')->daily();
+        $schedule->command('million:rates')->hourly();
     }
 
     /**
