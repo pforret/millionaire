613bf577ecc7128469274823e17fee4e000bcdf7
Peter Forret <peter@forret.com>
2022-03-09 23:54:24 +0100

    MOD: VERSION.blade.php sharethis.blade.php

--- resources/views/components/sharethis.blade.php
+++ resources/views/components/sharethis.blade.php
@@ -1,6 +1,6 @@
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-            <a href="https://twitter.com/intent/tweet?url=@yield('page_url')"
+            <a href="https://twitter.com/intent/tweet?url=@yield('page_url')&title=@yield('page_title')"
                target="_blank">
                 Share to Twitter
             </a>
